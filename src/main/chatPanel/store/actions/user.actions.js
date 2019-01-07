c0ac7533752bd859ce1a3ed0aecb00258f61763c
Billy Pons <pons.billy@outlook.com>
2019-01-07 13:23:31 -0800

    chat service

--- src/main/chatPanel/store/actions/user.actions.js
+++ src/main/chatPanel/store/actions/user.actions.js
@@ -1,4 +1,5 @@
 import axios from 'axios/index';
+import {chatService} from 'services'
 
 export const GET_USER_DATA = '[CHAT PANEL] GET USER DATA';
 
@@ -6,11 +7,25 @@ export function getUserData()
 {
     const request = axios.get('/api/chat/user');
 
-    return (dispatch) =>
+  /*   return (dispatch) =>
         request.then((response) =>
             dispatch({
                 type   : GET_USER_DATA,
                 payload: response.data
             })
-        );
+        ); */
+
+        return  (dispatch, getState) => {
+
+            const userId = getState().auth.login.Username;
+            const name = getState().auth.login.firstName ;
+            const avatar = getState().auth.user.data.photoURL;
+            (async () => {
+                let user = await chatService.getUserData(userId, name, avatar);
+                dispatch({
+                    type   : GET_USER_DATA,
+                    payload: user
+                });
+            })();
+        }
 }
--- src/services/chat/ChatService.js
+++ src/services/chat/ChatService.js
@@ -9,10 +9,10 @@ const BASE_API_URL='https://apifmsplus.jkdev.com';
 const BASE_MONGO_API_URL='https://apifmsplusplus_mongo.jkdev.com';
 const LOCALHOST_URL = "http://localhost:12217/";
 
-class ChatService {
+class chatService {
     getContactList =  (userId) => {
         return new Promise((resolve, reject) => {
-            axios_instance.get(`${BASE_MONGO_API_URL}/api/chat/contacts?id=`${userId})
+            axios_instance.get(`${LOCALHOST_URL}/api/chat/contacts?id=${userId}`)
                 .then( res => {
                     if(res.status===200) {
                         resolve(res.data);
@@ -29,13 +29,13 @@ class ChatService {
 
     getUserData = (userId, name, avatar) =>{
         const data = {
-            "id": [userId],
-            "name": [name],
-            "avatar_url": avatar,
+            "id": userId,
+            "name": name,
+            "custom_data": { "photo":  avatar, "mood": 'good'},
         };
 
         return new Promise((resolve, reject) => {
-            axios_instance.post(`${BASE_MONGO_API_URL}/api/chat/getuser`,data)
+            axios_instance.post(`${LOCALHOST_URL}/api/chat/getuser`,data)
                 .then( res => {
                     if(res.status===200) {
                         resolve(res.data);
@@ -52,13 +52,13 @@ class ChatService {
 
     openChat = (id, userId, name, avatar) => {
         const data = {
-            "id": [userId],
-            "name": [name],
-            "avatar_url": avatar,
+            "id": userId,
+            "name": name,
+            "custom_data":{ "photo":  avatar},
         };
 
         return new Promise((resolve, reject) => {
-            axios_instance.post(`${BASE_MONGO_API_URL}/api/chat/openchat?id=`${id}, data)
+            axios_instance.post(`${LOCALHOST_URL}/api/chat/openchat?id=${id}`, data)
                 .then( res => {
                     if(res.status===200) {
                         resolve(res.data);
@@ -74,3 +74,6 @@ class ChatService {
     }
 
 }
+
+const instance = new chatService();
+export default instance;
