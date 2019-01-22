8c7e26b97d7cd8c3dc2a3e298803475f8d0e856c
Super-coder <super.wanchiu@yandex.com>
2019-01-22 18:47:47 +0800

    fixed merge issue

--- src/services/index.js
+++ src/services/index.js
@@ -10,12 +10,10 @@ import _contactService from './customers/ContactService';
 import _chatService from './chat/ChatService';
 import _paymentService from './payment/paymentListService';
 import _dashboardService from './dashboard/dashboardService';
-<<<<<<< HEAD
 import _invoicePaymentService from './invoice/paymentsServices'
-
-=======
 import _homepageService from './home/HomepageService';
->>>>>>> cd74bb5ab055985c19dfbe72b76bae1ca0eed8d6
+
+
 export const authService = _authService;
 export const menuService = _menuService;
 export const franchiseesService = _franchiseesService;
@@ -23,13 +21,10 @@ export const leaseService = _leaseService;
 export const findersFeesService = _findersFeesService;
 export const initialService = _initialService;
 export const invoiceService = _invoiceService;
+export const invoicePaymentService = _invoicePaymentService;
 export const customersService = _customersService;
 export const chatService = _chatService;
 export const contactService = _contactService;
 export const paymentService = _paymentService;
 export const dashboardService = _dashboardService;
-<<<<<<< HEAD
-export const invoicePaymentService = _invoicePaymentService;
-=======
 export const homepageService = _homepageService;
->>>>>>> cd74bb5ab055985c19dfbe72b76bae1ca0eed8d6
