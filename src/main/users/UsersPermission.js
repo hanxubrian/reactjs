db6afea8e21e8e963434d0acec5e31f011171fc5
Super-coder <super.wanchiu@yandex.com>
2019-01-30 09:36:54 +0800

    added tab in users page permission

--- src/main/users/UsersPermission.js
+++ src/main/users/UsersPermission.js
@@ -1,35 +1,45 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import NoSsr from '@material-ui/core/NoSsr';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
 import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Exposure from '@material-ui/icons/Exposure';
-import AccountBalance from '@material-ui/icons/AccountBalance';
-import Visibility from '@material-ui/icons/Visibility';
-import LocationCity from '@material-ui/icons/LocationCity';
-import CardMembership from '@material-ui/icons/CardMembership';
-import RingVolume from '@material-ui/icons/RingVolume';
-import Language from '@material-ui/icons/Language';
-import Gamepad from '@material-ui/icons/Gamepad';
-import Build from '@material-ui/icons/Build';
-import MonetizationOn from '@material-ui/icons/MonetizationOn';
-import Portrait from '@material-ui/icons/Portrait';
-import VerticalSplit from '@material-ui/icons/VerticalSplit';
-import HowToVote from '@material-ui/icons/HowToVote';
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction/ListItemSecondaryAction";
 import Checkbox from "@material-ui/core/Checkbox/Checkbox";
 import Icon from "@material-ui/core/es/Icon/Icon";
 import menuOptions from "./menuOptions";
 
 
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
+    );
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+function LinkTab(props) {
+    return <Tab component="button" onClick={event => event.preventDefault()} {...props} />;
+}
+
 const styles = theme => ({
-    root: {
+    listRoot: {
         width: '100%',
         maxWidth: '100%',
-        //backgroundColor: theme.palette.background.paper,
+       // backgroundColor: theme.palette.background.paper,
     },
     nested: {
         paddingLeft: theme.spacing.unit * 4,
@@ -37,12 +47,19 @@ const styles = theme => ({
     textSpan: {
         marginLeft: "5px",
         marginRight: "5px"
+    },
+    tabRoot: {
+        flexGrow: 1,
+        border: '1px solid lightgrey',
+        marginTop: '10px'
+        //backgroundColor: theme.palette.background.paper,
     }
 });
 
 class UsersPermission extends React.Component {
     state = {
         open: true,
+        step: 0,
     };
 
     componentWillMount() {
@@ -53,66 +70,201 @@ class UsersPermission extends React.Component {
         this.setState(state => ({ open: !state.open }));
     };
 
+    handleTabChange = (event, step) => {
+        this.setState({ step });
+    };
+
     render() {
         const { classes } = this.props;
+        const { step } = this.state;
 
         return (
-            <div>
-               <List
-                    component="nav"
-                    className={classes.root}
-               >
-                   <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
-                       <ListItemText primary="Navigation Options" />
-                       <ListItemSecondaryAction>
-                           <span className={classes.textSpan}>View</span>
-                           <span className={classes.textSpan}>Create</span>
-                           <span className={classes.textSpan}>Edit</span>
-                           <span className={classes.textSpan}>Delete</span>
-                           <span className={classes.textSpan}>Execute</span>
-                       </ListItemSecondaryAction>
-                   </ListItem>
-                   {menuOptions.map(x => (
-                       <Fragment key={x.MenuId}>
-                           <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
-                               <ListItemIcon>
-                                   <Icon>{x.Icon}</Icon>
-                               </ListItemIcon>
-                               <ListItemText inset primary={x.Title} />
-                           </ListItem>
-                           { x.Children.map(x=>(
-                           <List key={x.MenuId} component="div" disablePadding>
-                               <ListItem className={classes.nested}>
-                                   <ListItemText inset primary={x.Title} />
-                                   <ListItemSecondaryAction>
-                                       <Checkbox
-                                           //onChange={this.handleToggle(value)}
-                                           checked={true}
-                                       />
-                                       <Checkbox
-                                           //onChange={this.handleToggle(value)}
-                                           checked={true}
-                                       />
-                                       <Checkbox
-                                           //onChange={this.handleToggle(value)}
-                                           checked={true}
-                                       />
-                                       <Checkbox
-                                           //onChange={this.handleToggle(value)}
-                                           checked={true}
-                                       />
-                                       <Checkbox
-                                           //onChange={this.handleToggle(value)}
-                                           checked={true}
-                                       />
-                                   </ListItemSecondaryAction>
-                               </ListItem>
-                           </List>
-                           ))}
-                       </Fragment>
-                   ))}
-               </List>
-            </div>
+            <NoSsr>
+                <div className={classes.tabRoot}>
+                    <AppBar color="default" position="static">
+                        <Tabs variant="fullWidth" value={step} onChange={this.handleTabChange}>
+                            <LinkTab icon={<Icon>web</Icon>} label="Web"/>
+                            <LinkTab icon={<Icon>phone</Icon>} label="Phone"/>
+                            <LinkTab icon={<Icon>tablet</Icon>} label="Tablet"/>
+                        </Tabs>
+                    </AppBar>
+                    {step === 0 && <TabContainer>
+                        <div>
+                            <List
+                                component="nav"
+                                className={classes.listRoot}
+                            >
+                                <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
+                                    <ListItemText primary="Navigation Options" />
+                                    <ListItemSecondaryAction>
+                                        <span className={classes.textSpan}>View</span>
+                                        <span className={classes.textSpan}>Create</span>
+                                        <span className={classes.textSpan}>Edit</span>
+                                        <span className={classes.textSpan}>Delete</span>
+                                        <span className={classes.textSpan}>Execute</span>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                {menuOptions.map(x => (
+                                    <Fragment key={x.MenuId}>
+                                        <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
+                                            <ListItemIcon>
+                                                <Icon>{x.Icon}</Icon>
+                                            </ListItemIcon>
+                                            <ListItemText inset primary={x.Title} />
+                                        </ListItem>
+                                        { x.Children.map(x=>(
+                                            <List key={x.MenuId} component="div" disablePadding>
+                                                <ListItem className={classes.nested}>
+                                                    <ListItemText inset primary={x.Title} />
+                                                    <ListItemSecondaryAction>
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                    </ListItemSecondaryAction>
+                                                </ListItem>
+                                            </List>
+                                        ))}
+                                    </Fragment>
+                                ))}
+                            </List>
+                        </div>
+                    </TabContainer>}
+                    {step === 1 && <TabContainer>
+                        <div>
+                            <List
+                                component="nav"
+                                className={classes.listRoot}
+                            >
+                                <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
+                                    <ListItemText primary="Navigation Options" />
+                                    <ListItemSecondaryAction>
+                                        <span className={classes.textSpan}>View</span>
+                                        <span className={classes.textSpan}>Create</span>
+                                        <span className={classes.textSpan}>Edit</span>
+                                        <span className={classes.textSpan}>Delete</span>
+                                        <span className={classes.textSpan}>Execute</span>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                {menuOptions.map(x => (
+                                    <Fragment key={x.MenuId}>
+                                        <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
+                                            <ListItemIcon>
+                                                <Icon>{x.Icon}</Icon>
+                                            </ListItemIcon>
+                                            <ListItemText inset primary={x.Title} />
+                                        </ListItem>
+                                        { x.Children.map(x=>(
+                                            <List key={x.MenuId} component="div" disablePadding>
+                                                <ListItem className={classes.nested}>
+                                                    <ListItemText inset primary={x.Title} />
+                                                    <ListItemSecondaryAction>
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                    </ListItemSecondaryAction>
+                                                </ListItem>
+                                            </List>
+                                        ))}
+                                    </Fragment>
+                                ))}
+                            </List>
+                        </div>
+                    </TabContainer>}
+                    {step === 2 && <TabContainer>
+                        <div>
+                            <List
+                                component="nav"
+                                className={classes.listRoot}
+                            >
+                                <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
+                                    <ListItemText primary="Navigation Options" />
+                                    <ListItemSecondaryAction>
+                                        <span className={classes.textSpan}>View</span>
+                                        <span className={classes.textSpan}>Create</span>
+                                        <span className={classes.textSpan}>Edit</span>
+                                        <span className={classes.textSpan}>Delete</span>
+                                        <span className={classes.textSpan}>Execute</span>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                {menuOptions.map(x => (
+                                    <Fragment key={x.MenuId}>
+                                        <ListItem button onClick={this.handleClick} style={{borderBottom: "1px solid lightgray"}}>
+                                            <ListItemIcon>
+                                                <Icon>{x.Icon}</Icon>
+                                            </ListItemIcon>
+                                            <ListItemText inset primary={x.Title} />
+                                        </ListItem>
+                                        { x.Children.map(x=>(
+                                            <List key={x.MenuId} component="div" disablePadding>
+                                                <ListItem className={classes.nested}>
+                                                    <ListItemText inset primary={x.Title} />
+                                                    <ListItemSecondaryAction>
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                        <Checkbox
+                                                            //onChange={this.handleToggle(value)}
+                                                            checked={true}
+                                                        />
+                                                    </ListItemSecondaryAction>
+                                                </ListItem>
+                                            </List>
+                                        ))}
+                                    </Fragment>
+                                ))}
+                            </List>
+                        </div>
+                    </TabContainer>}
+                </div>
+            </NoSsr>
+
         );
     }
 }
