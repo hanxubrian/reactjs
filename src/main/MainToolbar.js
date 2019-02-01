a8f5f5311399c0cdae92109962f0e230a0dc5289
evel.simmon <evel.simmon@gmail.com>
2019-02-02 03:35:07 +0800

    changed close btn action on admin page

--- src/main/MainToolbar.js
+++ src/main/MainToolbar.js
@@ -744,15 +744,7 @@ class MainToolbar extends Component {
                     />
                     </div>
                 )}
-                {/*{ true && (*/}
-                    {/*<div style={{position:"absolute"}}>*/}
-                    {/*<VersionUpgradeDialog*/}
-                        {/*show={true}*/}
-                        {/*version={"1.0.2"}*/}
-                        {/*note ={"this is new version note this is new version notethis is new version notethis is new version note"}*/}
-                    {/*/>*/}
-                    {/*</div>*/}
-                {/*)}*/}
+
 
                 {1 && (
 
