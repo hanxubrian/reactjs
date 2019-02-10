99c255f75263d81ef29cd8edb45226f393618feb
Super-coder <super.wanchiu@yandex.com>
2019-02-10 21:49:52 +0800

    added create,detail,update and delete service of users

--- src/services/users/userService.js
+++ src/services/users/userService.js
@@ -9,6 +9,94 @@ const BASE_MONGO_API_URL = 'https://apifmsplusplus_mongo.jkdev.com';
 
 class UserService {
     /**
+     * @method POST
+     * @param data
+     *
+     */
+    createUser = (data) => {
+        return new Promise((resolve, reject) => {
+            axios_instance.post(`${BASE_MONGO_API_URL}/v1/account/createuser`,data)
+                .then( res => {
+                    if(res.status===200) {
+                        resolve(res.data);
+                    }
+                    else if(res.status!==200){
+                        reject(res.data);
+                    }
+                })
+                .catch(error=>{
+                    resolve(error);
+                })
+        });
+    };
+
+    /**
+     * @method GET
+     *
+     */
+    getUserDetail = (id) => {
+        return new Promise((resolve, reject) => {
+            axios_instance.get(`${BASE_MONGO_API_URL}/v1/account/getuserById?id=${id}`)
+                .then( res => {
+                    if(res.status===200) {
+                        resolve(res.data);
+                    }
+                    else if(res.status!==200){
+                        reject(res.data);
+                    }
+                })
+                .catch(error=>{
+                    resolve(error);
+                })
+        });
+    };
+
+    /**
+     * @method POST
+     * @param id
+     * @param data
+     *
+     */
+    updateUser = (id,data) => {
+        return new Promise((resolve, reject) => {
+            axios_instance.post(`${BASE_MONGO_API_URL}/v1/account/updateuser?id=${id}`,data)
+                .then( res => {
+                    if(res.status===200) {
+                        resolve(res.data);
+                    }
+                    else if(res.status!==200){
+                        reject(res.data);
+                    }
+                })
+                .catch(error=>{
+                    resolve(error);
+                })
+        });
+    };
+
+    /**
+     * @method POST
+     * @param id
+     *
+     */
+    deleteUser = (id) => {
+        return new Promise((resolve, reject) => {
+            axios_instance.post(`${BASE_MONGO_API_URL}/v1/account/deleteuser?id=${id}`)
+                .then( res => {
+                    if(res.status===200) {
+                        resolve(res.data);
+                    }
+                    else if(res.status!==200){
+                        reject(res.data);
+                    }
+                })
+                .catch(error=>{
+                    resolve(error);
+                })
+        });
+    };
+
+    /**
      * @method GET
      *
      */
