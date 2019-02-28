7dba0693697bdbb6866970e5c085b4edd63d495c
evel.simmon <evel.simmon@gmail.com>
2019-03-01 05:11:02 +0800

    change chat button1

--- src/main/MainToolbar.js
+++ src/main/MainToolbar.js
@@ -6,28 +6,7 @@ import {withRouter} from 'react-router-dom';
 
 
 import classNames from 'classnames';
-import {
-    Avatar,
-    Button,
-    Fab,
-    Tabs,
-    Tab,
-    Icon,
-    AppBar,
-    ClickAwayListener,
-    IconButton,
-    Divider,
-    Paper,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Popover,
-    MenuItem,
-    Typography,
-    Hidden,
-    TextField
-} from '@material-ui/core';
+import {Avatar, Button,Fab,Tabs,Tab,Icon, AppBar,ClickAwayListener, IconButton,Divider, Paper,List, ListItem , ListItemIcon, ListItemText, Popover, MenuItem, Typography, Hidden} from '@material-ui/core';
 
 import * as quickPanelActions from 'main/quickPanel/store/actions';
 import * as authActions from '../auth/store/actions/login.actions';
@@ -38,9 +17,12 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Pusher from 'pusher-js';
 import moment from 'moment/moment';
+import ImageIcon from '@material-ui/icons/Image';
 import * as Actions from "./chatPanel/store/actions";
+
 import * as MainActions from 'store/actions';
 import ReactPlayer from 'react-player';
+import SystemNotification from './content/notifications/SystemNotification';
 import VersionUpgradeDialog from '../main/content/admin/AdminDialog/VersionUpgradeDialog';
 
 
@@ -66,17 +48,8 @@ const styles = theme => ({
             borderBottom: "solid 0px transparent !important"
         },'.MuiInput-underline-538:after': {
             borderBottom: "solid 0px transparent !important"
-        },
-        '& .period-select': {
-            minWidth: "120px",
-            paddingRight: "15px",
-            marginTop: "15px",
-        },
-        '& .period-select:before': {
-            borderBottom: "solid 0px transparent !important"
         }
     },
-
     notificationroot: {
         width: '100%',
         maxWidth: 600,
@@ -195,7 +168,7 @@ const styles = theme => ({
     },
     chattogglebtn:{
         position: 'fixed',
-        bottom: '20%',
+        bottom: '5px',
         // right: '90px',
         right: '15%',
         zIndex: 999999,
@@ -217,10 +190,7 @@ const styles = theme => ({
         height          : '386px',
         right           : '40px',
         top             : '85%',
-    },
-    input1: {
-        padding: '12px 6px'
-    },
+    }
 });
 
 class MainToolbar extends Component {
@@ -248,7 +218,6 @@ class MainToolbar extends Component {
         MSG                         : null,
         triggerF                    : false,
         checkloadingchatMSG         : true,
-        period: -1,
 
     };
 
@@ -262,12 +231,7 @@ class MainToolbar extends Component {
 
     handleChange = event => {
         this.setState({[event.target.name]: event.target.value});
-
-        if(event.target.name==='region')
-            this.props.setRegionId(event.target.value);
-
-        if(event.target.name==='period')
-            this.props.changeDefaultPeriod(event.target.value);
+        this.props.setRegionId(event.target.value);
     };
 
     handleClose = () => {
@@ -299,9 +263,6 @@ class MainToolbar extends Component {
         adminchannel.bind('on-admin', data => {
             this.setState({ adminMSG:data});
         });
-
-        if(this.props.defaultPeriod!==-1)
-            this.setState({period: this.props.defaultPeriod})
     }
     shownotification=()=>{
         // e.stopPropagation();
@@ -530,19 +491,19 @@ class MainToolbar extends Component {
     }
     chatPanelshowbtn=()=>{
         this.props.openChatPanel();
-    };
+    }
     mailnotificationchat=()=>{
         this.props.openChatPanel();
         this.shownotification();
-    };
+    }
     openchatpanel=()=>{
         this.props.changechatpanelshowstatus();
         this.props.openChatPanel();
-    };
+    }
     closechatpanel=()=>{
         this.props.changechatpanelshowstatus();
         this.props.closeChatPanel();
-    };
+    }
     handleContactClick = (contactId) => {
         if(contactId && contactId != null){
             let contactid =contactId;
@@ -636,7 +597,6 @@ class MainToolbar extends Component {
                                     open={this.state.open}
                                     onClose={this.handleClose}
                                     onOpen={this.handleOpen}
-                                    name='region'
                                     defaultValue={this.props.login.DefaultRegionId}
                                     value={parseInt(this.state.region)}
                                     onChange={this.handleChange}
@@ -651,26 +611,6 @@ class MainToolbar extends Component {
                                         );
                                     })}
                                 </Select>
-                            </FormControl>
-                            <FormControl className={classNames(classes.formControl) }>
-                                <TextField
-                                    select
-                                    name="period"
-                                    value={this.state.period}
-                                    onChange={this.handleChange}
-                                    className="period-select"
-                                    InputProps={{
-                                        name: "period",
-                                        classes: {
-                                            input: classes.input2,
-                                        },
-                                    }}
-                                    fullWidth
-                                >
-                                    {this.props.all_periods.map((p, index)=>{
-                                        return (<MenuItem key={index} value={p}>{p}</MenuItem>)
-                                    })}
-                                </TextField>
                             </FormControl>
                         </form>
                     )}
@@ -933,7 +873,7 @@ class MainToolbar extends Component {
 
                     <div className={classNames(classes.chattogglebtn,"chattogglebtnview")}>
                         <IconButton onClick={this.chatPanelshowbtn} style={{
-                            // visibility: 'hidden',
+                            visibility: 'hidden',
                             backgroundColor: '#3c93ec',
                             borderRadius: '50%',
                             color: 'white',
@@ -957,7 +897,6 @@ function mapDispatchToProps(dispatch)
         toggleQuickPanel                : quickPanelActions.toggleQuickPanel,
         logout                          : authActions.logoutUser,
         setRegionId                     : authActions.changeRegionId,
-        changeDefaultPeriod             : authActions.changeDefaultPeriod,
         openChatPanel                   : chatPanelActions.openChatPanel,
         getChat                         : chatPanelActions.getChat,
         loadedMenu                      : authActions.loadedMenu,
@@ -977,8 +916,6 @@ function mapStateToProps({auth,chatPanel,contactsApp,notification,billruns})
     return {
         user                    : auth.user,
         login                   : auth.login,
-        all_periods             : auth.login.all_periods,
-        defaultPeriod           : auth.login.defaultPeriod,
         chat                    : chatPanel.chat,
         IndividualChat          : chatPanel.IndividualChat,
         contacts                : chatPanel.contacts,
