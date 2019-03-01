b0168c8d6e5de9add69134d585fe3c4610f4a5a0
Super-coder <super.wanchiu@yandex.com>
2019-03-02 03:12:02 +0800

    updated chargeback modal UI

--- src/main/content/franchisees/chargebacks/ChargeBackSearchBar.js
+++ src/main/content/franchisees/chargebacks/ChargeBackSearchBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Icon, IconButton, Input, Paper, Button, Tooltip } from '@material-ui/core';
+import { Icon, IconButton, Input, Paper, Button } from '@material-ui/core';
 import classNames from 'classnames';
 
 import { withStyles } from "@material-ui/core";
@@ -10,65 +10,7 @@ import { bindActionCreators } from "redux";
 import connect from "react-redux/es/connect/connect";
 import * as Actions from 'store/actions';
 
-const hexToRgb = (hex) => {
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
-	} : null;
-};
-
 const styles = theme => ({
-	layoutTable: {
-		flexDirection: 'row',
-		'& .z-9999': {
-			height: 64
-		},
-		'& .-pageSizeOptions': {
-			display: 'none'
-		},
-		'& .openFilter': {
-			width: 'inherit'
-		},
-		'& .openSummary': {
-			width: 300
-		},
-		'& .p-12-impor': {
-			paddingLeft: '1.2rem!important',
-			paddingRight: '1.2rem!important',
-		},
-		'& .ReactTable .rt-noData': {
-			top: '250px',
-			border: '1px solid coral'
-		},
-		'& .ReactTable .rt-thead.-headerGroups': {
-			paddingLeft: '0!important',
-			paddingRight: '0!important',
-			minWidth: 'inherit!important'
-		},
-		'& .ReactTable.-highlight .rt-tbody .rt-tr:not(.-padRow):hover': {
-			background: 'rgba(' + hexToRgb(theme.palette.secondary.main).r + ',' + hexToRgb(theme.palette.secondary.main).g + ',' + hexToRgb(theme.palette.secondary.main).b + ', .8)',
-			color: 'white!important'
-		},
-		'& .ReactTable .rt-tbody': {
-			overflowY: 'scroll',
-			overflowX: 'hidden'
-		},
-		'& .ReactTable .rt-tr-group': {
-			flex: '0 0 auto'
-		},
-		'& .ReactTable .rt-thead .rt-th:nth-child(1)': {
-			justifyContent: 'center'
-		},
-		'& .ReactTable .rt-thead.-headerGroups .rt-th:nth-child(2)': {
-			width: 'inherit!important',
-			minWidth: 'inherit!important',
-		},
-		'& .ReactTable .rt-thead .rt-th:last-child': {
-			justifyContent: 'flex-end'
-		},
-	},
 	content: {
 		position: 'relative'
 	},
@@ -77,16 +19,6 @@ const styles = theme => ({
 		[theme.breakpoints.down('sm')]: {
 			width: '100%'
 		}
-	},
-	tableTheadRow: {
-		// backgroundColor: 'rgba(' + hexToRgb(theme.palette.primary.main).r + ',' + hexToRgb(theme.palette.primary.main).g + ',' + hexToRgb(theme.palette.primary.main).b +', .2)'
-		backgroundColor: theme.palette.primary.main
-	},
-	tableThEven: {
-		backgroundColor: 'rgba(' + hexToRgb(theme.palette.secondary.main).r + ',' + hexToRgb(theme.palette.secondary.main).g + ',' + hexToRgb(theme.palette.secondary.main).b + ', .3)'
-	},
-	tableTdEven: {
-		// backgroundColor: 'rgba(' + hexToRgb(theme.palette.secondary.main).r + ',' + hexToRgb(theme.palette.secondary.main).g + ',' + hexToRgb(theme.palette.secondary.main).b +', .1)'
 	},
 	filterPanelButton: {
 		backgroundColor: theme.palette.secondary.main,
@@ -109,15 +41,7 @@ const styles = theme => ({
 	},
 	imageIcon: {
 		width: 24
-	},
-	tableStriped: {
-		'& tbody tr:nth-of-type(odd)': {
-			backgroundColor: 'fade(' + theme.palette.primary.main + ', 0.03)',
-		},
-		'& tbody tr:nth-of-type(even)': {
-			backgroundColor: 'fade(' + theme.palette.primary.secondary + ', 0.03)',
-		},
-	},
+	}
 });
 
 const WAIT_INTERVAL = 1000;
@@ -125,7 +49,7 @@ const ENTER_KEY = 13;
 
 class ChargeBackSearchBar extends Component {
 
-	timer = null
+	timer = null;
 
 	constructor(props) {
 		super(props)
@@ -208,27 +132,16 @@ class ChargeBackSearchBar extends Component {
 						inputProps={{
 							'aria-label': 'Search'
 						}}
-					// onKeyDown={this.handleKeyDown}
 					/>
 
 					<IconButton
 						className={classNames(classes.button)}
-						// className={classNames(classes.button)}
 						aria-label="Add an alarm"
 						onClick={this.clearSearch}>
 						<Icon color="action">close</Icon>
 					</IconButton>
 
 				</Paper>
-
-				{/* <div className="flex items-center justify-end p-12">
-					<Button
-						onClick={this.props.toggleSummaryPanelChargeBacks}
-						aria-label="toggle summary panel"
-						className={classNames(classes.summaryPanelButton)}
-					>
-						<Icon>insert_chart</Icon>
-					</Button></div> */}
 			</div>
 		)
 	}
--- src/main/content/franchisees/chargebacks/ProcessModalForm.js
+++ src/main/content/franchisees/chargebacks/ProcessModalForm.js
@@ -107,127 +107,8 @@ const styles = theme => ({
         fontSize: 20
     }
 })
-function Transition(props) {
-	return <Slide direction="up" {...props} />;
-}
-
-//
-// table row edit command buttons
-//
-const AddButton = ({ onExecute }) => (
-	<div style={{ textAlign: 'center' }}>
-		{/* <Button
-			color="primary"
-			onClick={onExecute}
-			title="New Address"
-		>
-			New
-	  </Button> */}
-		<IconButton onClick={onExecute} title="Add New">
-			<NewIcon />
-		</IconButton>
-	</div>
-);
-
-const EditButton = ({ onExecute }) => (
-	<IconButton onClick={onExecute} title="Edit Payment Amount">
-		<EditIcon />
-	</IconButton>
-);
-
-const DeleteButton = ({ onExecute }) => (
-	<IconButton onClick={onExecute} title="Delete">
-		<DeleteIcon />
-	</IconButton>
-);
-
-const CommitButton = ({ onExecute }) => (
-	<IconButton onClick={onExecute} title="Save">
-		<SaveIcon />
-	</IconButton>
-);
-
-const CancelButton = ({ onExecute }) => (
-	<IconButton color="secondary" onClick={onExecute} title="Cancel">
-		<CancelIcon />
-	</IconButton>
-);
-
-const commandComponents = {
-	// add: AddButton,
-	edit: EditButton,
-	// delete: DeleteButton,
-	commit: CommitButton,
-	cancel: CancelButton,
-};
-
-const Command = ({ id, onExecute }) => {
-	const CommandButton = commandComponents[id];
-	return (
-		<CommandButton
-			onExecute={onExecute}
-		/>
-	);
-};
-
-const editing_cell_styles = theme => ({
-	cell: {
-		background: "#989898",
-		color: "white",
-		padding: 0,
-	}
-});
-const EditingHeaderCellComponentBase = props => {
-	return (<TableEditColumn.Cell {...props}
-
-	/>);
-};
-
-const EditingHeaderCellComponent = withStyles(editing_cell_styles, { name: "EditingCell" })(
-	EditingHeaderCellComponentBase
-);
-
-const EditingCellComponentBase = props => {
-	return (<TableEditColumn.Cell {...props}>
-		{React.Children.toArray(props.children)
-			.filter((child) => {
-				if (child.props.id === 'delete') {
-					// if (props.tableRow.row.id < 2) {
-					// return true;
-					// }
-					return false;
-				}
-				return true;
-			})}
-	</TableEditColumn.Cell>)
-};
-
-//
-// header cell style
-//
-const header_cell_styles = theme => ({
-	cell: {
-		background: "#989898",
-		color: "white",
-	}
-});
-const tableHeaderCellComponentBase = props => {
-	return (<TableHeaderRow.Cell {...props}
-
-	/>);
-};
-const tableHeaderCellComponent = withStyles(header_cell_styles)(
-	tableHeaderCellComponentBase
-);
 
 
-const EditingCellComponent = withStyles(editing_cell_styles, { name: "EditingCell" })(
-	EditingCellComponentBase
-);
-const getRowId = row => row.id;
-
-const CurrencyFormatter = ({ value }) => (<span>$ {parseFloat(`0${value}`).toLocaleString(undefined, { minimumFractionDigits: 2 })}</span>);
-const DateFormatter = ({ value }) => value.replace(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/, '$2/$3/$1');
 
 
 class ProcessModalForm extends React.Component {
@@ -235,149 +116,19 @@ class ProcessModalForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			columns: [
-				{
-					title: "Invoice No",
-					name: "InvoiceNo",
-					columnName: "InvoiceNo",
-					width: 200,
-					sortingEnabled: true,
-					filteringEnabled: true,
-					groupingEnabled: false,
-					editingEnabled: false,
-				},
-				{
-					title: "Invoice Date",
-					name: "InvoiceDate",
-					columnName: "InvoiceDate",
-					width: 300,
-					sortingEnabled: true,
-					filteringEnabled: true,
-					groupingEnabled: false,
-					editingEnabled: false,
-				},
-				{
-					title: "Due Date",
-					name: "DueDate",
-					columnName: "DueDate",
-					width: 300,
-					sortingEnabled: true,
-					filteringEnabled: true,
-					groupingEnabled: false,
-					editingEnabled: false,
-				},
-				{
-					title: "Invoice Amount",
-					name: "InvoiceAmount",
-					columnName: "InvoiceAmount",
-					width: 300,
-					sortingEnabled: true,
-					filteringEnabled: true,
-					groupingEnabled: false,
-					editingEnabled: false,
-				},
-				{
-					title: "Invoice Balance",
-					name: "InvoiceBalance",
-					columnName: "InvoiceBalance",
-					width: 300,
-					sortingEnabled: true,
-					filteringEnabled: true,
-					groupingEnabled: false,
-					editingEnabled: false,
-				},
-				{
-					title: "Amount to Apply",
-					name: "PaymentAmount",
-					columnName: 'PaymentAmount',
-					width: 300,
-					align: 'right',
-					wordWrapEnabled: true,
-					sortingEnabled: true,
-					filteringEnabled: true,
-					groupingEnabled: false,
-					editingEnabled: true,
-
-				}
-			],
-			columnsForReactDataGrid: [
-				{ key: "InvoiceNo", name: "Invoice No", editable: false },
-				{ key: "InvoiceDate", name: "Invoice Date", editable: false, formatter: DateFormatter },
-				{ key: "DueDate", name: "Due Date", editable: false, formatter: DateFormatter },
-				{ key: "DaysPastDue", name: "Days Past Due", editable: false, sortDescendingFirst: true },
-				{ key: "InvoiceAmount", name: "Invoice Amount", editable: false, formatter: CurrencyFormatter },
-				{ key: "InvoiceBalance", name: "Invoice Balance", editable: false, formatter: CurrencyFormatter },
-				{ key: "PaymentAmount", name: "Payment to Apply", editable: true, formatter: CurrencyFormatter }
-			],
-			rows: [],
-			currencyColumns: [
-				'InvoiceAmount', 'InvoiceBalance', 'PaymentAmount'
-			],
-			customerName: "",
-			customerNumber: "",
-
-			PaymentType: "Check",
-			ReferenceNo: "",
-			PaymentDate: new Date().toISOString().substr(0, 10),
-			PaymentNote: "",
-			PaymentAmount: 0,
-			overpayment: 0,
-            errorMsg: "",
-            period: moment().format('MM/YYYY'),
-            periods: null,
-			labelWidth: 0,
-			Notes: null,
-
-			paymentDlgPayloads: {
-				open: false,
-				paymentType: "Check",
-				paymentAmount: 0
-			},
 		}
-		this.props.getLogCallCustomerServiceTypes()
 	}
 
 	componentWillMount() {
-		console.log("componentWillMount")
-		this.setRowData(this.props.payments)
 
-		this.setState({
-			customerServiceTypes: this.props.lists.customerServiceTypes
-		})
 	}
 	componentDidMount() {
-		this.setState({
-			paymentDlgPayloads: this.props.paymentDlgPayloads,
-			PaymentAmount: this.props.paymentDlgPayloads.paymentAmount,
-			PaymentType: this.props.paymentDlgPayloads.paymentType
-		})
-	}
-	UNSAFE_componentWillReceiveProps(nextProps) {
-		if (nextProps.payments !== this.props.payments) {
-			console.log("componentWillReceiveProps payments")
-			this.setRowData(nextProps.payments)
-		}
-		if (!_.isEqual(nextProps.activePaymentRows, this.props.activePaymentRows)) {
-			console.log("componentWillReceiveProps activePaymentRows", nextProps.activePaymentRows, this.props.activePaymentRows)
-			this.setRowData(this.props.payments, nextProps.activePaymentRows)
-		}
-		if (!_.isEqual(nextProps.paymentDlgPayloads, this.props.paymentDlgPayloads)) {
-			this.setState({
-				paymentDlgPayloads: nextProps.paymentDlgPayloads,
-				PaymentAmount: nextProps.paymentDlgPayloads.paymentAmount,
-				PaymentType: nextProps.paymentDlgPayloads.paymentType
-			})
-		}
+
 	}
 
 	handleClose = () => {
 		this.props.showProcessModalForm(false)
 	};
-
-	handleChangeChecked = name => event => {
-		this.setState({ [name]: event.target.checked });
-	};
-
 	handleChange = name => event => {
 		this.setState({
 			[name]: event.target.value,
@@ -385,63 +136,10 @@ class ProcessModalForm extends React.Component {
 		});
 	};
 
-	commitChanges = ({ added, changed, deleted }) => {
-		let { rows } = this.state;
-		if (added) {
-			const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
-			rows = [
-				...rows,
-				...added.map((row, index) => ({
-					id: startingAddedId + index,
-					...row,
-				})),
-			];
-		}
-		if (changed) {
-			rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
-		}
-		if (deleted) {
-			const deletedSet = new Set(deleted);
-			rows = rows.filter(row => !deletedSet.has(row.id));
-		}
-		this.setState({ rows });
-	}
 
-	setRowData(payments, activePaymentRows = this.props.activePaymentRows) {
 
-		if (!payments || payments.Regions === undefined || payments.Regions.length < 1)
-			return [];
-		let res = [...payments.Regions[0].Payments]
 
-		res.forEach((x, index) => {
-			x.CustomerNameNo = `${x.CustomerName} - ${x.CustomerNo}`;
-			x.id = index
-		})
-		res = res.filter(x => {
-			return activePaymentRows.indexOf(x.id) > -1
-		})
-		console.log("setRowData", res);
-		if (res.length > 0) {
-			this.setState({
-				customerName: res[0].CustomerName,
-				customerNumber: res[0].CustomerNo,
-			})
-		}
-		this.setState({ rows: res })
-	}
 
-	onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
-		this.setState(state => {
-			const rows = state.rows.slice();
-			for (let i = fromRow; i <= toRow; i++) {
-				rows[i] = { ...rows[i], ...updated };
-			}
-
-			return {
-				rows
-			};
-		});
-	};
 
 	autoDistribute = () => {
 		this.setState(state => {
@@ -467,182 +165,14 @@ class ProcessModalForm extends React.Component {
 		})
 	}
 
-	clearDistribute = () => {
-		this.setState(state => {
-			const rows = state.rows.slice();
-			for (let i = 0; i < rows.length; i++) {
-				rows[i] = { ...rows[i], PaymentAmount: 0 };
-			}
-			// this.checkValidations('', '', rows)
-
-			return {
-				rows: rows,
-				overpayment: 0,
-			}
-		})
-	}
-	isNonEmptyPayment(rows) {
-		//
-		// check row for payment amount
-		//
-		let existPositivePayment = false
-		let existPaymentsGreaterThanBalance = false
-
-		rows.forEach(x => {
-			existPositivePayment = existPositivePayment || 0 < x.PaymentAmount
-			existPaymentsGreaterThanBalance = existPaymentsGreaterThanBalance || x.InvoiceBalance < x.PaymentAmount
-		})
-		if (!existPositivePayment) {
-			return "Neither of payments amount is settled"
-		}
-		if (existPaymentsGreaterThanBalance) {
-			return "One or more Payment Amounts is greater than Invoice Balance"
-		}
-		return ""
-	}
-
-	checkLiveValidations(name = "", value = "", rows = this.state.rows, ) {
-
-		if (name === "PaymentType" && !value || name !== "PaymentType" && !this.state.PaymentType) {
-			this.setState({ errorMsg: "Payment type not selected" })
-		} else if (name === "ReferenceNo" && value <= 0 || name !== "ReferenceNo" && this.state.ReferenceNo <= 0) {
-			this.setState({ errorMsg: "ReferenceNo is invalid" })
-		} else if (name === "PaymentDate" && !value || name !== "PaymentDate" && !this.state.PaymentDate) {
-			this.setState({ errorMsg: "Payment date not selected" })
-		} else if (name === "PaymentAmount" && value <= 0) {
-			this.setState({ errorMsg: "Amount is invalid" })
-		} else {
-
-			let totalPaymentAmount = 0
-			let floatPaymentAmount = parseFloat(`0${name === "PaymentAmount" ? value : this.state.PaymentAmount}`)
-
-			this.state.rows.forEach(x => {
-				totalPaymentAmount += parseFloat(`0${x.PaymentAmount}`)
-			})
-			console.log("floatPaymentAmount", "totalPaymentAmount", floatPaymentAmount, totalPaymentAmount)
-			if (floatPaymentAmount < totalPaymentAmount) {
-				this.setState({
-					errorMsg: "Total payment is greater than payment to apply",
-					overpayment: 0,
-				})
-			} else {
-				this.setState({
-					overpayment: floatPaymentAmount - totalPaymentAmount
-				})
-			}
-
-			const isNonEmptyPayment = this.isNonEmptyPayment(rows)
-			console.log("isNonEmptyPayment", isNonEmptyPayment)
-			if (isNonEmptyPayment) {
-				this.setState({ errorMsg: isNonEmptyPayment })
-			}
-			else {
-				this.setState({ errorMsg: "" })
-				return true
-			}
-
-		}
-		return false
-	}
-
-	getOverpaymentAmount(rows = this.state.rows, paymentAmount = this.state.PaymentAmount) {
-		let totalPaymentAmount = 0
-		paymentAmount = parseFloat(`0${paymentAmount}`)
-		rows.forEach(x => {
-			totalPaymentAmount += parseFloat(`0${x.PaymentAmount}`)
-		})
-		return paymentAmount - totalPaymentAmount
-	}
-
-	checkValidations(rows = this.state.rows, paymentAmount = this.state.paymentAmount) {
-		if (!this.state.PaymentType) {
-			this.setState({
-				errorMsg: "Payment type not selected",
-				overpayment: this.getOverpaymentAmount(rows, paymentAmount),
-			})
-		} else if (!this.state.ReferenceNo || !this.state.ReferenceNo.toString().trim()) {
-			this.setState({
-				errorMsg: "ReferenceNo is invalid",
-				overpayment: this.getOverpaymentAmount(rows, paymentAmount),
-			})
-		} else if (!this.state.PaymentDate) {
-			this.setState({
-				errorMsg: "Payment date not selected",
-				overpayment: this.getOverpaymentAmount(rows, paymentAmount),
-			})
-		} else if (this.state.PaymentAmount <= 0) {
-			this.setState({
-				errorMsg: "Amount is invalid",
-				overpayment: this.getOverpaymentAmount(rows, paymentAmount),
-			})
-
-		} else if (!this.checkIfAllZeroPaymentsValidation(rows)) {
-			this.setState({ errorMsg: "Neither of payments amount is settled" })
-		} else if (!this.checkIfAPaymentGreaterThanBalanceValidation(rows)) {
-			this.setState({ errorMsg: "One or more Payment Amounts is greater than Invoice Balance" })
-		} else if (!this.checkIfAllPaymentsGreaterThanAmountValidation(rows, paymentAmount)) {
-			this.setState({ errorMsg: "Sum of payments is greater than applied one." })
-		} else {
-			return true
-		}
-		return false
-	}
-
-	checkIfAllZeroPaymentsValidation(rows) {
-		let existPositivePayment = false
-		rows.forEach(x => {
-			existPositivePayment = existPositivePayment || 0 < x.PaymentAmount
-		})
-		return existPositivePayment
-	}
-
-	checkIfAPaymentGreaterThanBalanceValidation(rows) {
-		let existPaymentsGreaterThanBalance = false
-		rows.forEach(x => {
-			existPaymentsGreaterThanBalance = existPaymentsGreaterThanBalance || x.InvoiceBalance < x.PaymentAmount
-		})
-		return !existPaymentsGreaterThanBalance
-	}
-
-	checkIfAllPaymentsGreaterThanAmountValidation(rows, paymentAmount = this.state.PaymentAmount) {
-		let totalPaymentAmount = 0
-		paymentAmount = parseFloat(`0${paymentAmount}`)
-		rows.forEach(x => {
-			totalPaymentAmount += parseFloat(`0${x.PaymentAmount}`)
-		})
-		return totalPaymentAmount <= paymentAmount
-	}
-
 
 	handleChangeEmailNotesTo = (event, value) => {
 		this.setState({ EmailNotesTo: value });
 	};
 
-	handleCreateCustomerCollection = () => {
-		const { activeCustomer } = this.props
-		const { SpokeWith, Comments, CallBack, ActionValue, StatusBy } = this.state
-		const data = {
-			"cust_name": activeCustomer.Data.cus_name,
-			"call_com": Comments,
-			"sys_cust": 0,
-			"cust_no": activeCustomer.Data.cust_no,
-			"call_date": moment().format("MM/DD/YYYY"),
-			"call_time": moment().format("HH:mm:ss"),
-			"call_stat": 0,
-			"stat_otr": StatusBy,
-			"spoke_with": SpokeWith,
-			"call_back": CallBack,
-			"call_btime": moment().format("HH:mm:ss"),
-			"action": 0,
-			"action_otr": ActionValue
-		}
-		this.props.customerCollectionCreate(this.props.regionId, data)
-	}
 
 	render() {
 		const { classes } = this.props;
-        const { customerServiceTypes } = this.state;
-
 		return (
 			<div className={classes.root}>
 				<Dialog
@@ -660,32 +190,7 @@ class ProcessModalForm extends React.Component {
                     </DialogTitle>
 					<DialogContent>
 						<GridContainer style={{ alignItems: 'center' }} className={classNames(classes.formControl,"mt-24")}>
-							<GridItem xs={12} sm={4} md={4} className="flex flex-row ">
-								<Select
-									classes={{
-										selectMenu: classNames(classes.inputMenu1),
-									}}
-									label="Period"
-									name="period"
-									variant="outlined"
-									value={this.state.period}
-									onChange={this.handleChange}
-                                    fullWidth
-									input={
-										<OutlinedInput
-											labelWidth={this.state.labelWidth}
-											name="period"
-											id="period"
-										/>
-									}
-									className={classes.textField}
-									MenuProps = {{
-										classes:{paper: classes.dropdownMenu},
-									}}
-								>
-								</Select>
-							</GridItem>
-                            <GridItem xs={12} sm={4} md={4} className="flex flex-row ">
+                            <GridItem xs={12} sm={6} md={6} className="flex flex-row ">
 								<TextField
 									margin="none"
 									label="Month"
@@ -708,7 +213,7 @@ class ProcessModalForm extends React.Component {
 									}}
 								/>
 							</GridItem>
-                            <GridItem xs={12} sm={4} md={4} className="flex flex-row ">
+                            <GridItem xs={12} sm={6} md={6} className="flex flex-row ">
 								<TextField
 									margin="none"
 									label="Year"
@@ -742,7 +247,6 @@ class ProcessModalForm extends React.Component {
 
 					<DialogActions>
 						<Button variant="contained" onClick={this.handleCreateCustomerCollection} color="primary" className={classNames("pl-24 pr-24 mb-12 mr-12")}>Save</Button>
-						<Button variant="contained" onClick={this.handleClose} color="primary" className={classNames("pl-24 pr-24 mb-12 mr-24")}>Close</Button>
 					</DialogActions>
 				</Dialog>
 			</div>
@@ -752,35 +256,15 @@ class ProcessModalForm extends React.Component {
 
 function mapDispatchToProps(dispatch) {
 	return bindActionCreators({
-		openPaymentDialog: Actions.openPaymentDialog,
-		createAccountReceivablePayment: Actions.createAccountReceivablePayment,
-
 		showProcessModalForm: Actions.showProcessModalForm,
-
-		getLogCallCustomerServiceTypes: Actions.getLogCallCustomerServiceTypes,
-
-		customerCollectionCreate: Actions.customerCollectionCreate,
 	}, dispatch);
 }
 
 function mapStateToProps({ customers, accountReceivablePayments, auth, chargebacks }) {
 	return {
 		regionId: auth.login.defaultRegionId,
-		bOpenPaymentDialog: accountReceivablePayments.bOpenPaymentDialog,
-		activePaymentRows: accountReceivablePayments.activePaymentRows,
-
-		payments: accountReceivablePayments.ACC_payments,
-
-		filterParam: accountReceivablePayments.filterParam,
-		searchText: accountReceivablePayments.searchText,
-
-		paymentDlgPayloads: accountReceivablePayments.paymentDlgPayloads,
-
-        logCallModalForm: customers.logCallModalForm,
         processModalForm: chargebacks.processModalForm,
-		lists: customers.lists,
-        activeCustomer: customers.activeCustomer,
-        all_regions: auth.login.all_regions,
+
 	}
 }
 
